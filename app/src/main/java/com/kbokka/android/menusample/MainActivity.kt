940c20127c2efe30e41424fef0f3956c780ca9c0
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-07 21:58:32 +0900

    Add create curry list method

--- app/src/main/java/com/kbokka/android/menusample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/menusample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kbokka.android.menusample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
@@ -27,6 +28,11 @@ class MainActivity : AppCompatActivity() {
     lvMenu.onItemClickListener = ListItemClickListener()
   }
 
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_options_menu_list, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
   private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
     val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
 
@@ -42,6 +48,27 @@ class MainActivity : AppCompatActivity() {
           "name" to "ハンバーグ定食${it}",
           "price" to (820 + (it * 10)),
           "desc" to "手ごねハンバーグ${it}にサラダ、ご飯とお味噌汁が付きます。"
+        )
+      )
+    }
+    return menuList
+  }
+
+  private fun createCurryList(): MutableList<MutableMap<String, Any>> {
+    val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+
+    menuList.add(
+      mutableMapOf("name" to "ビーフカレー", "price" to 700, "desc" to "特選スパイスをきかせた国産ビーフ100%のカレーです。")
+    )
+    menuList.add(
+      mutableMapOf("name" to "ポークカレー", "price" to 710, "desc" to "特選スパイスをきかせた国産ポーク100%のカレーです。")
+    )
+    Array(8) { it + 1 }.forEach {
+      menuList.add(
+        mutableMapOf(
+          "name" to "ポークカレー${it}",
+          "price" to (710 + it * 10),
+          "desc" to "特選スパイス${it}をきかせた国産ポーク100%のカレーです。"
         )
       )
     }
