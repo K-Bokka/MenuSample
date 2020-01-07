7fd6d9cc95948be656b480dc2f068713db34e380
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-07 21:33:59 +0900

    Add create teisholu list method

--- app/src/main/java/com/kbokka/android/menusample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/menusample/MainActivity.kt
@@ -5,8 +5,29 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
+    val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+
+    menuList.add(
+      mutableMapOf("name" to "から揚げ定食", "price" to 800, "desc" to "若鶏のから揚げにサラダ、ご飯とお味噌汁が付きます。")
+    )
+    menuList.add(
+      mutableMapOf("name" to "ハンバーグ定食", "price" to 810, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+    )
+    Array(8) { it + 1 }.forEach {
+      menuList.add(
+        mutableMapOf(
+          "name" to "ハンバーグ定食${it}",
+          "price" to (820 + (it * 10)),
+          "desc" to "手ごねハンバーグ${it}にサラダ、ご飯とお味噌汁が付きます。"
+        )
+      )
     }
+    return menuList
+  }
 }
